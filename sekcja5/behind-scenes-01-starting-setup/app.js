8ad8a9146bb9c0e30057f7d7635949fb4023c123
Aleksandra <akrysinska@emyad.com>
2022-03-09 13:27:47 +0100

    cd

--- sekcja5/behind-scenes-01-starting-setup/app.js
+++ sekcja5/behind-scenes-01-starting-setup/app.js
@@ -15,9 +15,37 @@ const app = Vue.createApp({
       console.log(this.$refs.userText.value);
     },
   },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeupdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  }
 });
 
 app.mount('#app');
+
+setTimeout(function () {
+  app.unmount();
+}, 3000);
 
 const app2 = Vue.createApp({
   template: `
