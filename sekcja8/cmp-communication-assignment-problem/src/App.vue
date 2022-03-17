ac6dd2bf9fbddb81a7406b228eb72a785f39820c
Aleksandra <akrysinska@emyad.com>
2022-03-17 11:09:37 +0100

    cd

---
+++ sekcja8/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <active-user :username="user.name" :userage="user.age"></active-user>
+    <user-data></user-data>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Max Schwarzmuller",
+        age: 31,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+</style>
--- sekcja8/cmp-communication-assignment-problem/src/components/UserData.vue
+++ sekcja8/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -1,9 +1,19 @@
 <template>
   <section>
-    <input type="text" placeholder="Your name" />
-    <input type="text" placeholder="Your age" />
+    <form action="">
+      <input type="text" placeholder="Your name" v-model="enteredName" />
+      <input type="text" placeholder="Your age" v-model="enteredAge" />
+      <button>Set User Data</button>
+    </form>
   </section>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+};
 </script>
