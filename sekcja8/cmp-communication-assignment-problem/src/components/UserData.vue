a4da14186e0e425ac6f4019f62155a4fc7253fff
Aleksandra <akrysinska@emyad.com>
2022-03-25 15:55:06 +0100

    cd

--- sekcja8/cmp-communication-assignment-problem/src/components/UserData.vue
+++ sekcja8/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <form action="">
+    <form @submit.prevent="submitData">
       <input type="text" placeholder="Your name" v-model="enteredName" />
       <input type="text" placeholder="Your age" v-model="enteredAge" />
       <button>Set User Data</button>
@@ -9,11 +9,17 @@
 </template>
 <script>
 export default {
+  emits: ["set-data"],
   data() {
     return {
       enteredName: "",
       enteredAge: "",
     };
+  },
+  methods: {
+    submitData() {
+      this.$emit("set-data", this.enteredName, this.enteredAge);
+    },
   },
 };
 </script>
