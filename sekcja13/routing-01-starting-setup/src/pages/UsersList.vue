24df56beaf97ee712cc9cc62cf7d456712243d8b
Aleksandra <akrysinska@emyad.com>
2022-04-04 12:05:52 +0200

    cd

---
+++ sekcja13/routing-01-starting-setup/src/pages/UsersList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <button @click="confirmInput">Confirm</button>
+    <button @click="saveChanges">Save Changes</button>
+    <ul>
+      <user-item
+        v-for="user in users"
+        :key="user.id"
+        :name="user.fullName"
+        :role="user.role"
+      ></user-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import UserItem from '../components/users/UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  data() {
+    return { changesSaved: false };
+  },
+  methods: {
+    confirmInput() {
+      this.$router.push('/teams');
+      // this.$router.back();
+    },
+    saveChanges() {
+      this.changesSaved = true;
+    },
+  },
+  beforeRouteEnter(to, from, next) {
+    next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm(
+        'Are you sure? You got unsaved changes!'
+      );
+      next(userWantsToLeave);
+    }
+  },
+  unmounted() {
+    console.log('unmounted');
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
