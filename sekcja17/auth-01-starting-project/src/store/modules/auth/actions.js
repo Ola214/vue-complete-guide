25865d71bbe369c523ef83151c92b8478bbbc23f
Aleksandra <akrysinska@emyad.com>
2022-04-08 11:07:26 +0200

    cd

--- sekcja17/auth-01-starting-project/src/store/modules/auth/actions.js
+++ sekcja17/auth-01-starting-project/src/store/modules/auth/actions.js
@@ -36,13 +36,34 @@ export default {
             throw error;
         }
 
+        const expiresIn = responseData.expiresIn * 1000;
+        const expirationDate = new Date().getTime() + expiresIn;
+
+        localStorage.setItem('token', responseData.idToken);
+        localStorage.setItem('userId', responseData.localId);
+
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
             tokenExpiration: responseData.expiresIn
         })
     },
+    tryLogin(context) {
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+
+        if(token && userId) {
+            context.commit('setUser', {
+                token: token,
+                userId: userId,
+                tokenExpiration: null
+            });
+        }
+    },
     logout(context) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+
         context.commit('setUser', {
             token: null,
             userId: null,
